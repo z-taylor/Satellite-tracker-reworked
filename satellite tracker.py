1a03b8d2782b79b6c4da4d99917e60faa7633290
Zachary Taylor <ztaylor4590@gmail.com>
2024-08-27 22:42:00 -0400

    asdasdasdasdasd

--- satellite tracker.py
+++ satellite tracker.py
@@ -4,7 +4,6 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QWidget, QDial
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QTimer, QThread, QEventLoop, Signal
 import os
-import platform
 
 
 class main(QMainWindow):
@@ -17,15 +16,9 @@ class main(QMainWindow):
             self.ui.actionPreferences.triggered.connect(self.open_preferences)
             self.ui.actionRadio.triggered.connect(self.open_radio)
             self.ui.actionRotator.triggered.connect(self.open_rotator)
-            #self.TCPthread = TcpThread()
-            #self.closeEvent = self.on_close
             #timer1 = QTimer(self)
             #timer1.timeout.connect(self.update_sat_info)
             #timer1.start(200)
-            #timer2 = QTimer(self)
-            #timer2.timeout.connect(self.run_tcp_thread)
-            #timer2.start(100)
-            #self.buttons()
       def savePrefs(self):
             print("save")
       def cancelPrefs(self):
@@ -57,13 +50,6 @@ class main(QMainWindow):
             loop = QEventLoop()
             loop.exec()
      
-"""     def run_tcp_thread(self):
-          if not self.TCPthread.isRunning():
-               self.TCPthread.received.connect(self.update_sat_info)
-               self.TCPthread.start()
-     def on_close(self, event):
-     def update_sat_info(self, data=None):"""
-
 if __name__ == "__main__":
       QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
       app=QApplication(sys.argv)
