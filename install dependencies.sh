ca443017ec6c952818a44d362b9dcdbd5789bdfd
z-taylor <120347442+z-taylor@users.noreply.github.com>
2024-09-04 15:17:03 -0400

    Update install dependencies.sh

--- install dependencies.sh
+++ install dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
-pip3 install --break-system-packages PySide6 geocoder requests pyhigh pyproj skyfield
+pip3 install --break-system-packages PySide6 geocoder requests pyhigh pyproj skyfield python-dateutil
 echo "Press any key to continue..."
 read
