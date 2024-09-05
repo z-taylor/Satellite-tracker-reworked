5a45dbb8c95e2ca6ff311441347cc96ad82c7fbf
Zachary Taylor <120347442+z-taylor@users.noreply.github.com>
2024-09-05 15:23:02 -0400

    added pyhamlib so I can add radio and rotator control

--- install dependencies.sh
+++ install dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
-pip3 install --break-system-packages PySide6 geocoder requests pyhigh pyproj skyfield python-dateutil
+pip3 install --break-system-packages PySide6 geocoder requests pyhigh pyproj skyfield python-dateutil pyhamlib
 echo "Press any key to continue..."
 read
