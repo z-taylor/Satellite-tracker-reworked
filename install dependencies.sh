5c0852bb4f5557c51ddbc3226101a3da41f96587
z-taylor <120347442+z-taylor@users.noreply.github.com>
2024-09-07 19:23:20 +0000

    updated install scripts
    Works on a new install of linux now

--- install dependencies.sh
+++ install dependencies.sh
@@ -2,8 +2,8 @@
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
-if ! command_exists python3; then
-    echo "Python is not installed. Installing....."
+install_python_and_pip() {
+    echo "Installing Python and pip....."
     if command_exists apt-get; then
         sudo apt-get update
         sudo apt-get install -y python3 python3-pip
@@ -12,10 +12,22 @@ if ! command_exists python3; then
     elif command_exists pacman; then
         sudo pacman -Syu --noconfirm python python-pip
     else
-        echo "Package manager is not supported. Please manually install python"
+        echo "Package manager is not supported. Please manually install Python and pip"
         exit 1
     fi
-    echo "Python installed"
+    echo "Python and pip installed"
+}
+if ! command_exists python3; then
+    echo "Python is not installed"
+    install_python_and_pip
+else
+    echo "Python is already installed"
+fi
+if ! command_exists pip3; then
+    echo "pip is not installed. Installing....."
+    install_python_and_pip
+else
+    echo "pip is already installed"
 fi
 python3 -m pip install --upgrade pip --break-system-packages
 pip3 install --break-system-packages PySide6 geocoder requests pyhigh pyproj skyfield python-dateutil pyhamlib
